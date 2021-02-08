0cba8a72ca5db3acfa58156ad128b560a327e53f
DarenPalmer <darenpalmer22@gmail.com>
2021-02-08 22:11:54 +0100

    created StartupMenu

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+//TODO: add initial launch screen using shared_preferences
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: StartupMenu(),
     );
   }
 }
@@ -28,32 +29,32 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [HexColor("#ffafbd"), HexColor("#ffc3a0")])),
-      child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            title: Container(
-              margin: EdgeInsets.only(top: 15),
-              child: Center(
-                  child: Text(
-                "News API",
-                style: TextStyle(color: Colors.black, fontSize: 30),
-              )),
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Tech News",
+              style: TextStyle(color: Colors.black, fontSize: 30),
             ),
-            backgroundColor: Colors.transparent,
-            shadowColor: Colors.transparent,
           ),
-          body: NewsList()),
-    );
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        body: NewsList(
+          topic: "politics",
+        ));
   }
 }
 
+// ignore: must_be_immutable
 class NewsList extends StatefulWidget {
+  String topic;
+  var date = DateTime.now();
+  String url;
+  NewsList({
+    this.topic,
+  });
+
   @override
   _NewsListState createState() => _NewsListState();
 }
@@ -61,47 +62,161 @@ class NewsList extends StatefulWidget {
 class _NewsListState extends State<NewsList> {
   @override
   Widget build(BuildContext context) {
+    widget.url =
+        "http://newsapi.org/v2/top-headlines?country=us&category=${widget.topic}&apiKey=f5d7d2d6ea364425a88b196b2af3d53f";
     return FutureBuilder(
-      future: http.get(
-          "http://newsapi.org/v2/top-headlines?country=us&from=2020-12-31&sortBy=publishedAt&apiKey=f5d7d2d6ea364425a88b196b2af3d53f"),
+      future: http.get(widget.url),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.data != null) {
           Map<String, dynamic> data = jsonDecode(snapshot.data.body);
-          return ListView.builder(
-            padding: EdgeInsets.all(8),
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () async =>
-                    await canLaunch(data["articles"][index]["url"])
-                        ? await launch(data["articles"][index]["url"])
-                        : throw "Could not launch url",
-                child: Container(
-                    margin: EdgeInsets.only(bottom: 8),
-                    padding: EdgeInsets.only(
-                        right: 40, left: 17, top: 17, bottom: 17),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.black, width: 3),
-                      color: Colors.white,
-                    ),
-                    child: Column(children: [
-                      Text("${data["articles"][index]["title"]}",
-                          overflow: TextOverflow.clip,
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w500)),
-                      Text("${data["articles"][index]["description"]}",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w500)),
-                    ])),
-              );
+          return RefreshIndicator(
+            onRefresh: () async {
+              setState(() {
+                widget.topic = widget.topic;
+              });
             },
+            child: ListView.builder(
+              padding: EdgeInsets.all(8),
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () async =>
+                      await canLaunch(data["articles"][index]["url"])
+                          ? await launch(data["articles"][index]["url"])
+                          : throw "Could not launch url",
+                  child: Container(
+                      margin: EdgeInsets.only(bottom: 12),
+                      padding: EdgeInsets.all(17),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 4),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Column(children: [
+                        data["articles"][index]["title"] == null
+                            ? Container()
+                            : Text("${data["articles"][index]["title"]}",
+                                overflow: TextOverflow.clip,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w500)),
+                        data["articles"][index]["description"] == null
+                            ? Container()
+                            : Text("${data["articles"][index]["description"]}",
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w500)),
+                      ])),
+                );
+              },
+            ),
           );
         } else {
           return Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+}
+
+class StartupMenu extends StatefulWidget {
+  List<String> selected;
+
+  @override
+  _StartupMenuState createState() => _StartupMenuState();
+}
+
+class _StartupMenuState extends State<StartupMenu> {
+  void _apply() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String topics = pref.getString("topics");
+    print(topics);
+    await pref.setString("topics", "test");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size deviceDimensions = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Topics", style: TextStyle(fontSize: 30)),
+          Container(
+            width: deviceDimensions.width / 1.3,
+            height: deviceDimensions.height / 1.7,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(12.0),
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: TopicForm(),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class SelectionButton extends StatefulWidget {
+  final Icon icon;
+  final String topic;
+  bool value = false;
+
+  SelectionButton({this.icon, this.topic});
+
+  @override
+  _SelectionButtonState createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SwitchListTile(
+        title: Text(widget.topic),
+        secondary: widget.icon,
+        value: widget.value,
+        onChanged: (value) {
+          setState(() {
+            widget.value = value;
+            print("Debug: Switch value = ${widget.value}");
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TopicForm extends StatefulWidget {
+  List<SelectionButton> arr = [
+    SelectionButton(
+      icon: Icon(Icons.computer),
+      topic: "Tech",
+    ),
+  ];
+  @override
+  TopicFormState createState() => TopicFormState();
+}
+
+class TopicFormState extends State<TopicForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(child: widget.arr[0]),
+        Spacer(),
+        FlatButton(
+          onPressed: () {
+            print(widget.arr[0].value);
+          },
+          child: Text("Done"),
+          color: Colors.blue,
+        )
+      ],
     );
   }
 }
